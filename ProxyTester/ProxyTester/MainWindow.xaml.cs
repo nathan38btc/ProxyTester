deece8e8bec0c654d41ca41b99c9369ae2bf4c2f
nathan38btc <natmaure354@gmail.com>
2022-02-12 20:15:39 +0100

    Interface grafique
    projet Visual Studio

---
+++ ProxyTester/ProxyTester/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProxyTester
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> listeProxy = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AjoutProxy newWindow = new AjoutProxy(this.listeProxy);
+            Window.GetWindow(newWindow).Show();
+        }
+
+    }
+}
---
+++ ProxyTester/ProxyTester/Proxy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyTester
+{
+    class Proxy
+    {
+        #region attributs
+        private string ip;
+        private string port;
+        private string adresse;
+        private string password;
+        #endregion
+
+        #region constructor
+
+        public Proxy(string donneesProxy)
+        {
+            string[] DonneeExploitable = donneesProxy.Split(';');
+            this.ip = DonneeExploitable[0];
+            this.port = DonneeExploitable[1];
+            this.adresse = DonneeExploitable[2];
+            this.password = DonneeExploitable[3];
+        }
+        #endregion
+
+        #region Properties
+        public string Ip
+        {
+            get { return ip; }
+            set { ip = value; }
+        }
+
+        public string Port
+        {
+            get { return port; }
+            set { port = value; }
+        }
+
+        public string Adresse
+        {
+            get { return adresse; }
+            set { adresse = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        #endregion
+
+    }
+}
